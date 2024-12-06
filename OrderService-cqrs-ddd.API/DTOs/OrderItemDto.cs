633857a2565b8e2927b3112c0346e0a97078cfb7
NicoFilips <git@nicofilips.ch>
2024-12-06 19:35:46 +0100

    feat: add minimal API Endpoint

---
+++ OrderService-cqrs-ddd.API/DTOs/OrderItemDto.cs
@@ -0,0 +1,7 @@
+﻿namespace OrderService_cqrs_ddd.API.DTOs;
+
+public record OrderItemDto
+{
+    public Guid ProductId { get; set; }
+    public int Quantity { get; set; }
+}
---
+++ OrderService-cqrs-ddd.API/Endpoints/MinimalApi/OrdersMinimalAPI.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using OrderService_cqrs_ddd.Application.Commands.CancelOrder;
+using OrderService_cqrs_ddd.Application.Commands.CreateOrder;
+
+namespace OrderService_cqrs_ddd.API.Endpoints.MinimalAPI;
+
+public static class OrdersMinimalApi
+{
+    public static IEndpointRouteBuilder MapOrdersEndpoints(this IEndpointRouteBuilder endpoints)
+    {
+        // POST: /api/orders
+        endpoints.MapPost("/api/orders", async (CreateOrderCommand command, IMediator mediator) =>
+        {
+            Guid result = await mediator.Send(command);
+            return Results.Ok(result);
+        });
+
+        // DELETE: /api/orders/{id}
+        endpoints.MapDelete("/api/orders/{id}", async (Guid id, IMediator mediator) =>
+        {
+            await mediator.Send(new CancelOrderCommand { OrderId = id });
+            return Results.NoContent();
+        });
+
+        return endpoints;
+    }
+}
